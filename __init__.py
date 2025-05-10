2f370761dce533d8b1e5b46ac8cb954205ac777f
PedrCorreia <correia.18.pedro@gmail.com>
2025-05-10 12:27:57 +0100

    fixed comfyui_init

--- __init__.py
+++ __init__.py
@@ -2,7 +2,7 @@ NODE_CLASS_MAPPINGS = {}
 IMPORT_ERROR_MESSAGE = "PIC nodes: failed to import"
 
 try:
-    from .comfy.tools import PlotNode  # Ensure the file name matches the actual file
+    from .comfy.tools import PlotNode
     NODE_CLASS_MAPPINGS["SignalPlotter"] = PlotNode
 except ImportError as e:
     print(f"{IMPORT_ERROR_MESSAGE} SignalPlotter: ImportError - {e}")
@@ -13,14 +13,36 @@ try:
     from .comfy.signalprocessing import (
         MovingAverageFilter,
         SignalFilter,
-        SignalPLL,
-        SignalThresholdFilter,  # Import the missing threshold filter
+
     )
     NODE_CLASS_MAPPINGS["MovingAverageFilter"] = MovingAverageFilter
     NODE_CLASS_MAPPINGS["SignalFilter"] = SignalFilter
-    NODE_CLASS_MAPPINGS["SignalPLL"] = SignalPLL
-    NODE_CLASS_MAPPINGS["SignalThresholdFilter"] = SignalThresholdFilter  # Add the threshold filter node
+
 except ImportError as e:
     print(f"{IMPORT_ERROR_MESSAGE} SignalProcessing Nodes: ImportError - {e}")
 except Exception as e:
     print(f"{IMPORT_ERROR_MESSAGE} SignalProcessing Nodes: {type(e).__name__} - {e}")
+
+try:
+    from .comfy import ECGNode
+    NODE_CLASS_MAPPINGS["ECGNode"] = ECGNode
+except ImportError as e:
+    print(f"{IMPORT_ERROR_MESSAGE} ECGNode: ImportError - {e}")
+except Exception as e:
+    print(f"{IMPORT_ERROR_MESSAGE} ECGNode: {type(e).__name__} - {e}")
+
+try:
+    from .comfy  import RRNode
+    NODE_CLASS_MAPPINGS["RRNode"] = RRNode
+except ImportError as e:
+    print(f"{IMPORT_ERROR_MESSAGE} RRNode: ImportError - {e}")
+except Exception as e:
+    print(f"{IMPORT_ERROR_MESSAGE} RRNode: {type(e).__name__} - {e}")
+
+try:
+    from .comfy  import EDANode
+    NODE_CLASS_MAPPINGS["EDANode"] = EDANode
+except ImportError as e:
+    print(f"{IMPORT_ERROR_MESSAGE} EDANode: ImportError - {e}")
+except Exception as e:
+    print(f"{IMPORT_ERROR_MESSAGE} EDANode: {type(e).__name__} - {e}")
